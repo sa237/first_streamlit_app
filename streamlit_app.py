fc12e9a9ea838dcdc91cbe6f4bd8396b3725d267
sa237 <46806956+sa237@users.noreply.github.com>
2023-01-06 10:04:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title('New Text 2')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
